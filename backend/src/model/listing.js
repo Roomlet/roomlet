2a9bfa13643a34386a03fed90ae3060d4fb51014
Stephanie Dover <doverstephaniem@gmail.com>
2017-08-14 10:54:39 -0700

    updated listing model

--- backend/src/model/listing.js
+++ backend/src/model/listing.js
@@ -12,6 +12,7 @@ const listingSchema = new Schema({
   petsAllowed: { type: Boolean },
   nonSmoking: { type: Boolean },
   comment: { type: String },
+  parkingSpaces: { type: Number },
 })
 
 const Listing = Mongoose.model('listing', listingSchema)
