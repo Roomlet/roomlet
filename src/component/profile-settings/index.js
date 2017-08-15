a996328110bbf1a200697b84b703d813eeee7a12
Kyle Aardal <aardal.kyle@gmail.com>
2017-08-15 11:14:32 -0700

    now succefully makes put requests to user profile on auth0 API

--- src/component/profile-settings/index.js
+++ src/component/profile-settings/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { profileUpdateRequest } from '../../action/profile-actions.js'
 
 class ProfileSettings extends React.Component {
   constructor(props) {
     super(props)
-    this.state = props.profile ? { ...props.profile } : { bio: '' }
+    this.state = props.profile
+      ? { ...props.profile }
+      : { bio: '', token: props.token }
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -22,6 +25,9 @@ class ProfileSettings extends React.Component {
   handleSubmit(e) {
     e.preventDefault()
     //TODO: make a patch request with token and id to api/v2/users/id
+    console.log('profile submit')
+    this.props.profileUpdate(this.state)
+    console.log('profile submit after')
   }
 
   render() {
@@ -46,6 +52,8 @@ export const mapStateToProps = state => ({
   userId: state.userId,
 })
 
-export const mapDispatchToProps = dispatch => ({})
+export const mapDispatchToProps = dispatch => ({
+  profileUpdate: profile => dispatch(profileUpdateRequest(profile)),
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProfileSettings)
